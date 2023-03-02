a4363d8385f573a08cb558e840e3b3d369ee5bac
sistemas <you@example.com>
2023-03-02 16:39:27 -0500

    Se agrego eld iagrama de flujo, programa y el READM

---
+++ Ejercicio_1/README.md
@@ -0,0 +1,7 @@
+# EJERCICIO 1
+Programa para leer cordenadas de un plano cartesiano e indicar el cuadrante
+# DISEÑO
+
+## Diagrama de flujo
+
+![Diagrama de flujo](diagrama.png "Diagrama de flujo")
--- Ejercicio_1/ejercicio_1.py
+++ Ejercicio_1/ejercicio_1.py
@@ -1,23 +1,36 @@
-# Programa que lea las coordenadas cartesianas (x, y) de un punto en el plano y calcule el cuadrante al cual pertenece el punto y si el punto está sobre un eje también debe indicarlo.
+# Programa para leer cordenadas de un plano cartesiano e indicar el cuadrante
 
-print("--------------------------------")
-print("------ INGRESE UN NUMERO -------")
-print("--------------------------------")
+
+print("-------------------------------------------------------")
+print("Programa para indicar el cuadrante de  unas coordenadas")
+print("-------------------------------------------------------")
 
 #input
 
-x = float(input("Ingrese la coordenada x: "))
-y = float(input("Ingrese la coordenada y: "))
+x = int(input("Digite la coordenada x: "))
+y = int(input("Digite la coordenada y: "))
 
-#processing
+# processing
 
-if x == 0 and y == 0:
-    print("El punto se encuentra en el origen.")
-if x > 0 and y > 0:
-    print("El punto se encuentra en el I cuadrante.")
-if x < 0 and y > 0:
-    print("El punto se encuentra en el 	II cuadrante.")
-if x < 0 and y < 0:
-    print("El punto se encuentra en el 	III cuadrante.")
+if x == 0:
+    if y == 0:
+        msj = ("Las coordenadas corresponden al origen")
+    else:
+        msj = ("Las coordenadas corresponden al eje y")
 else:
-    print("El punto se encuentra en el IV cuadrante.")
+    if y == 0:
+        msj = ("Las coordenadas corresponden al eje x")
+    else:
+        if x > 0:
+            if y > 0:
+                msj = ("Las coordenadas corresponden al cuadrante 1")
+            else:
+                msj = ("Las coordenadas corresponden al cuadrante 4")
+        else:
+            if y < 0:
+                msj = ("Las coordenadas corresponden al cuadrante 3")
+            else:
+                msj = ("Las coordenadas corresponden al cuadrante 2")
+# output
+
+print (msj)
